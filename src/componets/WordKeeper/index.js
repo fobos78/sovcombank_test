ecc7fa0bcfb82b296a5926acfc4918e4f9a8f7c7
Artem <marsov78@mail.ru>
2020-09-19 15:05:23 +0300

    release 2

--- src/componets/WordKeeper/index.js
+++ src/componets/WordKeeper/index.js
@@ -5,90 +5,92 @@ import './WordKeeper.css';
 
 function WordKeeper() {
   const { starWords, setStarWord } = useContext(ThemeContext);// избранное
-  const [word, setWord] = useState('');// используем для контроля input
-  const [wordSearch, setWordSearch] = useState('');// слово по которому делаем запрос
-  const [abcWords, setAbcWords] = useState([]);// список по алфавиту
-  const [i, setI] = useState(0);// количество слов в списке
-  const [inputCh, setInputCh] = useState(0);// реакция на ввод в input
-  const [count, setCount] = useState(0);// количество слов в списке
-  const [arrWords, setArrWords] = useState([]);// массив с предворительным списком
-  const [nearWords, setNearWords] = useState(['']);// массив с близкими словами
   const [abc, setAbc] = useState(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);// массив алфавита
+  const [word, setWord] = useState('');
+  const [wordsSearch, setWordsSearch] = useState([]);
+  const [words, setWords] = useState([]);
+  const [push, setPush] = useState(0);
+  const arr = [];
+  const arrActualWords = [];
+  let count = 0;
 
-  async function searchWords() {
+  async function searchWords(i) {
     const obj = {};
-    const respons = await fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${wordSearch}?key=7f3bd985-8595-4004-b953-672f4501bf55`);
+    const respons = await fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${words[i]}?key=7f3bd985-8595-4004-b953-672f4501bf55`);
     const result = await respons.json();
-    console.log('result', result);
-    if (result[0].meta.id) {
+    if (result[0] === undefined) {
+      i++;
+    } else
+    if (result[0].meta) {
       obj.word = result[0].meta.id;
       obj.fl = result[0].fl;
       obj.shortdef = result[0].shortdef[0];
-      // console.log('result', result);
-      setAbcWords(result);
-      if (i < 5) { // количество слов в списке
-        setArrWords([...arrWords, obj]);
-        setI(i + 1);
-        setWordSearch(obj.word + abc[count]);
+      if (!(arr.find((el) => el.word === obj.word))) {
+        arr.push(obj);
       }
     } else {
-      console.log('resultNo', result);
-      setNearWords([...result]);
-      setAbcWords(['']);
-      setCount(count + 1);
-      for (let z = 0; z < 2; z++) {
-        const respons1 = await fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${nearWords[z]}?key=7f3bd985-8595-4004-b953-672f4501bf55`);
-        const result1 = await respons1.json();
-        console.log('result111', result1);
-        console.log('test');
-        if (result1[z].meta.id) {
-          obj.word = result1[z].meta.id;
-          obj.fl = result1[z].fl;
-          obj.shortdef = result1[z].shortdef[0];
-          setAbcWords(result1);
-          if (i < 5) { // количество слов в списке
-            setArrWords([...arrWords, obj]);
-            setI(i + 1);
-          }
-        }
-        //setWordSearch(obj.word + abc[count]);
+      console.log('result----', result);
+      arrActualWords.push(result);
+      const respons1 = await fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${arrActualWords[0][count]}?key=7f3bd985-8595-4004-b953-672f4501bf55`);
+      const result1 = await respons1.json();
+      obj.word = result1[0].meta.id;
+      obj.fl = result1[0].fl;
+      obj.shortdef = result1[0].shortdef[0];
+      // count = Math.floor(Math.random() * 5);
+      count++;
+      if (!(arr.find((el) => el.word === obj.word))) {
+        arr.push(obj);
       }
     }
+
+    const arr4 = arr.sort((a, b) => {
+      if (a.word.toLowerCase() > b.word.toLowerCase()) {
+        return 1;
+      }
+      if (a.word.toLowerCase() < b.word.toLowerCase()) {
+        return -1;
+      }
+      return 0;
+    });
+    setWordsSearch([...arr4]);
   }
 
   useEffect(() => {
-    searchWords();
-  }, [inputCh, wordSearch]);
+    if (push) {
+      for (let i = 0; i < 10; i++) {
+        searchWords(i);
+      }
+    }
+  }, [push]);
   function inputChange(event) {
     setWord(event.target.value);
-    setWordSearch(event.target.value);
-    setArrWords([]);
-    setCount(0);
-    setI(0);
-    setInputCh(inputCh + 1);
+    setWords([]);
+    count = 0;
+  }
+  function clickPush() {
+    let str = word;
+    const arr1 = [];
+    for (let i = 0; i < 26; i++) {
+      arr1.push(str);
+      setWords([...arr1]);
+      str = word + abc[i];
+    }
+    setPush(push + 1);
   }
   return (
     <div className="WordKeeper">
       <div>
         Search word:
         <input onChange={inputChange} value={word} />
-        <button type="button" onClick={() => setWordSearch(word)}>Push</button>
+        <button type="button" onClick={clickPush}>Push</button>
       </div>
-      {arrWords[0] && (
+      {/* <p>{console.log('words>>>', words)}</p>
+      <p>{console.log('wordsSearch>>>', wordsSearch)}</p> */}
+      {wordsSearch[0] && (
         <>
-          {/* <div>
-            <span><b>{abcWords[0].hwi.hw}</b></span>
-&nbsp;
-            <span><i>{abcWords[0].fl}</i></span>
-&nbsp;
-            <span>{abcWords[0].shortdef[0]}</span>
-          </div> */}
-          <h3>{console.log('arrWords>>>', { ...arrWords[i] }.word)}</h3>
           {
-            arrWords.map((el, x) => (
+            wordsSearch.map((el) => (
               <div>
-                <h3>{console.log('arrWords>>>', { ...arrWords[x] }.word)}</h3>
-
                 <span><b>{{ ...el }.word}</b></span>
 &nbsp;
                 <span><i>{{ ...el }.fl}</i></span>
